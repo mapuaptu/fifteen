ac3f7c4962b478ddc2477ed493701ce6fc6e2cf8
mapuaptu <mapuaptu@gmail.com>
2021-01-10 12:12:12 +0400

    Base project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,16 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/airbnb',
+    '@vue/typescript',
+  ],
 };
